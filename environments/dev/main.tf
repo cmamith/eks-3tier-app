caaa759c6de5af41d234646ea1aa2da3ab2f4325
amith <cmamith@gmail.com>
2025-07-05 23:46:26 +0800

    Add VPC module usage in dev environment

---
+++ environments/dev/main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name                = "dev"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
+  availability_zones  = ["ap-southeast-1a", "ap-southeast-1b"]
+}
