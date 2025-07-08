d93b109bdbc24c43fdf4f73d11db1ab502158303
amith <cmamith@gmail.com>
2025-07-08 23:14:04 +0800

    changed to public

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,7 +12,8 @@ module "eks" {
   source = "../../modules/eks"
 
   name       = "dev"
-  subnet_ids = module.vpc.private_subnet_ids
+  subnet_ids = module.vpc.public_subnet_ids
+  vpc_id     = module.vpc.vpc_id
 
   desired_capacity = 2
   min_size         = 1
