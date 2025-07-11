7721b0e575cd524f340b2632e1e00d2ed2aba94e
amith <cmamith@gmail.com>
2025-07-11 21:55:18 +0800

    addding the outputs.tf in the file

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,15 @@
+output "rds_identifier" {
+  value = module.rds.db_identifier
+}
+
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
+
+output "rds_instance_id" {
+  value = module.rds.rds_instance_id
+}
+
+output "rds_arn" {
+  value = module.rds.rds_arn
+}
