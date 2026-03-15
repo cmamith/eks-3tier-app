fb9f40695bd72f026d624f90f679c265e0589552
amith <cmamith@gmail.com>
2026-03-16 02:11:16 +0800

    testing backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "interview-project-s3-20021988"
+    bucket         = "interview-project-devex-s3-20021988"
     key            = "eks/dev/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "interiew-project-locktable"
